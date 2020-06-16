fdd94a239aba8ac9ba463859febdc8daa0b78270
zewbare <zewbare@gmail.com>
2020-06-16 17:12:05 +0530

    student registration ready

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify
+from flask_restful import Api
+from flask_jwt_extended import JWTManager
+from resources.student import StudentRegister
+
+app = Flask(__name__)
+app.config['PROPAGATE_EXCEPTIONS']=True
+app.config['JWT_SECRET_KEY']='coscskillup'
+api = Api(app)
+jwt = JWTManager(app)
+
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return jsonify({
+        'error':'authorization required',
+        "description":"Request does not contain an access token"
+    }), 401
+
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+    return jsonify({
+        'error':'invalid token',
+        "description":"Signature verification failed"
+    }), 401
+
+api.add_resource(StudentRegister, '/studentregister')
+
+if __name__ == '__main__':
+    app.run()
---
+++ resources/student.py
@@ -0,0 +1,45 @@
+from flask_restful import Resource, reqparse
+from db import query
+from flask_jwt_extended import create_access_token, jwt_required
+from werkzeug.security import safe_str_cmp
+
+class StudentRegister(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+
+        parser.add_argument('srollno', type = str, required = True, help = 'roll no cannot be left blank')
+        parser.add_argument('spassword', type = str, required = True, help = 'password cannot be left blank')
+        parser.add_argument('sname', type = str, required = True, help = 'name cannot be left blank')
+        parser.add_argument('sdept', type = str, required = True, help = 'dept cannot be left blank')
+        parser.add_argument('syear', type = int, required = True, help = 'year cannot be left blank')
+        parser.add_argument('semail', type = str, required = True, help = 'email cannot be left blank')
+        parser.add_argument('sphone', type = str, required = True, help = 'phone no cannot be left blank')
+        parser.add_argument('spgname', type = str, required = True, help = 'Parent/Guardian name cannot be left blank')
+        parser.add_argument('spgemail', type = str, required = True, help = 'Parent/Guardian email cannot be left blank')
+
+        data = parser.parse_args()
+
+        try:
+            isAlreadyPresent = query(f"""SELECT * FROM students WHERE srollno = {data['srollno']}""", return_json = False)
+            if len(isAlreadyPresent) > 0:
+                return {"message":"Student with given roll no already exists"},400
+        except:
+            return {"message":"Error inserting into STUDENTS"},500
+
+        try:
+            query(f"""INSERT INTO students VALUES (
+                                                            '{data['srollno']}',
+                                                            '{data['spassword']}',
+                                                            '{data['sname']}',
+                                                            '{data['sdept']}',
+                                                            {data['syear']},
+                                                            '{data['semail']}',
+                                                            '{data['sphone']}',
+                                                            {data['spgemail']},
+                                                            '{data['spgphone']}'
+                                                            )"""
+                                                            )
+        except:
+            return {"message":"Error inserting into STUDENTS"},500
+        
+        return {"message":"Student successfully registered"},201
